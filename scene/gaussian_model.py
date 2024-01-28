65c62125ec7027211b689a9a5c68f0e3111c1142
renaissanceee <st179481@stud.uni-stuttgart.de>
2024-01-28 21:16:13 +0100

    gmm

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -28,7 +28,7 @@ class GaussianModel:
     def setup_functions(self):
         # 从scale rotation重建3D-Gaussian的协方差矩阵
         def build_covariance_from_scaling_rotation(scaling, scaling_modifier, rotation):
-            L = build_scaling_rotation(scaling_modifier * scaling, rotation)
+            L = build_scaling_rotation(scaling_modifier * scaling, rotation)#[points,3,3]
             actual_covariance = L @ L.transpose(1, 2)
             symm = strip_symmetric(actual_covariance)
             return symm
@@ -60,16 +60,6 @@ class GaussianModel:
         self.percent_dense = 0
         self.spatial_lr_scale = 0
         self.setup_functions()
-        # self.mlp_=nn.Sequential(
-        #     nn.Linear(num_old, hidden),
-        #     nn.Linear(hidden, num_new)
-        # )
-        # self.mlp_xyz = nn.Linear(3, 6)
-        # self.mlp_features_dc = nn.Linear(3, 6)
-        # self.mlp_features_rest = nn.Linear(3, 6)
-        # self.mlp_opacity = nn.Linear(1, 2)
-        # self.mlp_scales = nn.Linear(3, 6)
-        # self.mlp_rotations = nn.Linear(4, 8)
 
 
     def capture(self):
@@ -232,6 +222,61 @@ class GaussianModel:
         # print(self._opacity.size())#[29495, 1]->
         # print(self._scaling.size())#[29495, 3]->
         # print(self._rotation.size())#[29495, 4]->
+    def gmm_fitting(self, gmm_degree):
+        # fit 1 Gaussian-> 2 Gaussian (if gmm_degree=2)
+        # mean,covariance-> double for fitting
+        # features, opacity -> copy
+        import torch.distributions as D
+        from torch.distributions.multivariate_normal import MultivariateNormal
+        from torch.distributions.mixture_same_family import MixtureSameFamily
+
+
+        # 1.mean, covariance
+        mean_pre=self.get_xyz[0]
+        covariance_matrix_pre = self.get_covariance(scaling_modifier=1)[0] # s,q -> [182686, 6]
+        cov_matrix = torch.zeros((3, 3), dtype=torch.float)
+
+
+        # distr_pre = MultivariateNormal(mean_pre, covariance_matrix_pre)
+        # cov_matrix = covariance_matrix_pre.view(-1, 3, 2)
+        cov_matrix[0, 1] = covariance_matrix_pre[0]
+        cov_matrix[0, 2] = covariance_matrix_pre[1]
+        cov_matrix[1, 2] = covariance_matrix_pre[2]
+        cov_matrix[0, 0] = covariance_matrix_pre[3]
+        cov_matrix[1, 1] = covariance_matrix_pre[4]
+        cov_matrix[2, 2] = covariance_matrix_pre[5]
+        # Mirror the upper triangular part to complete the symmetric matrix
+        cov_matrix = cov_matrix + cov_matrix.T - torch.diag(cov_matrix.diag())
+
+
+        # Construct Gaussian Mixture Model in 3D consisting of 2 components
+        mix = D.Categorical(torch.ones(gmm_degree,))
+        # print(Normal(torch.randn(mean_pre.shape[0], gmm_degree, 3), torch.rand(mean_pre.shape[0], gmm_degree, 3)).shape)
+        comp = D.Independent(D.Normal(mean_pre, cov_matrix), 1)
+        gmm = MixtureSameFamily(mix, comp)
+        num_samples =1000
+        samples = gmm.sample([num_samples])
+
+        means = samples.mean(dim=0)  # Mean of the components
+        covariances = torch.cat(
+            [gmm.component_distribution.covariance_matrix.unsqueeze(0) for gmm in samples.components], dim=0)
+
+        print(means.size())
+        print(covariances.size())
+        # 2.Gaussian-fitting
+        # mean(center) ->concat
+        aaaaa
+        # mean_1 =
+        # mean_2 =
+        # self._xyz = concat(mean_1,mean_2)
+        # # covariance_matrix(s,q) -> assump same scaling
+        # L = build_scaling_rotation(scaling, rotation)
+        # actual_covariance = L @ L.transpose(1, 2)
+
+
+
+
+
 
     def training_setup(self, training_args):
         # 为3D-Gaussian的各组参数创建optimizer和lr
